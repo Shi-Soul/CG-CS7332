42c5ace9e62d5acdb27ac6ee942976ca72e09c9b
Shi-Soul <993520026@qq.com>
2025-06-07 22:46:08 +0800

    Tuning Color

--- proj/main.py
+++ proj/main.py
@@ -10,27 +10,38 @@ def main():
 
     material = Material(
         ka=0.1,
-        kd=0.5,
-        ks=0.7,
-        n=1.2,
-        a=0.1,
-        b=1.0,
-        c=1.0,
-        color=np.array([0.5, 0.5, 1])
+        kd=0.7,
+        ks=0.8,
+        n=10.0,
+        a=1.0,
+        b=0.35,
+        c=0.44,
+        color=np.array([0.2, 0.2, 0.8])
+    )
+    material_blue = Material(
+        ka=0.2,
+        kd=0.7,
+        ks=0.8,
+        n=10.0,
+        a=1.0,
+        b=0.35,
+        c=0.44,
+        color=np.array([0.2, 0.2, 0.8])
     )
 
     cube = Object(
         mesh=
+            # Sphere(np.array([0, 0, 0]), 0.5, 32),
             Subdivide(
                 Cube(np.array([0, 0, 0]), 1), 
             3),
         material=material
     )
 
-    light = Light(np.array([-0.7, -0.6, 0.6]), 1, np.array([1, 1, 1]))
+    light = Light(np.array([-0.7, -0.6, 0.8]), 1, np.array([1, 1, 1]))
 
-    observer = np.array([-5, -3.5, 3])/3
-    camera = Camera(observer, -observer, np.array([0, 0, 1]), 52, 1.5, 800)
+    observer = np.array([-5, -3.5, 3])/2
+    camera = Camera(observer, -observer, np.array([0, 0, 1]), 60, 1.5, 800)
 
     image_size = GetImageSize(camera)
 
